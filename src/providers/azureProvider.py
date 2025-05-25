548293f621437a8bdd1edd5efe0d8f26de5870bb
enigma-vsc <vsc3110@gmail.com>
2025-05-25 20:02:49 +0530

    VM list and RG list available

--- src/providers/azureProvider.py
+++ src/providers/azureProvider.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from azure.identity import DefaultAzureCredential
 from azure.mgmt.resource import ResourceManagementClient
+from azure.mgmt.compute import ComputeManagementClient
 
 def load_env(env_file_path: str):
     if not os.path.exists(env_file_path):
@@ -16,19 +17,35 @@ def get_resource_client() -> ResourceManagementClient:
     credential = DefaultAzureCredential()
     return ResourceManagementClient(credential, subscription_id)
 
+def get_compute_client() -> ComputeManagementClient:
+    subscription_id = os.getenv("AZURE_SUBSCRIPTION_ID")
+    if not subscription_id:
+        raise ValueError("AZURE_SUBSCRIPTION_ID is not set in environment.")
+    credential = DefaultAzureCredential()
+    return ComputeManagementClient(credential, subscription_id)
+
 def list_resource_groups(client: ResourceManagementClient):
+    print("Listing Resource Groups:")
+    print("====================================")
     for rg in client.resource_groups.list():
         print(f"- {rg.name}")
 
+def list_virtual_machines(client: ComputeManagementClient):
+    print("Listing Virtual Machines:")
+    print("====================================")
+    vms = client.virtual_machines.list_all()
+    for vm in vms:
+        print(vm.name)
 
-# def main():
-#     env_file_path = os.getenv("ENV_FILE_PATH", ".env")
-#     load_env(env_file_path)
-#     print(f"Using environment file: {env_file_path}")
-#     print("Listing Azure Resource Groups:")
-#     print("====================================")
-#     client = get_resource_client()
-#     list_resource_groups(client)
 
-# if __name__ == "__main__":
-#     main()
+def main():
+    env_file_path = os.getenv("ENV_FILE_PATH", ".env")
+    load_env(env_file_path)
+    print(f"Using environment file: {env_file_path}")
+    client = get_resource_client()
+    compute_client = get_compute_client()
+    list_resource_groups(client)
+    list_virtual_machines(compute_client)
+
+if __name__ == "__main__":
+    main()
